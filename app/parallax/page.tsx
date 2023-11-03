30275b1bcdd6bc4b5041bc9d5af9bbaec9b08ea6
Johannes Langosch <johannes.langosch@googlemail.com>
2023-11-03 18:50:49 +0100

    02 database

---
+++ app/parallax/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Parallax = (props: Props) => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-sea bg-no-repeat bg-fixed bg-cover">
+      <h1>Parallax</h1>
+    </div>
+  );
+};
+
+export default Parallax;
